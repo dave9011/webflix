f829d284e61cfe6a8e85a8866efe66bbd604f5c8
David Hernandez <hrdavidl@gmail.com>
2022-08-13 22:53:09 -0400

    Define the intial routes to be used in the app (login, register, home and watch)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,37 @@
 import './app.scss';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Watch from './pages/watch/Watch';
+import {
+  Navigate,
+  Redirect,
+  Routes,
+  Route,
+} from "react-router-dom"
 
 const App = () => {
-  return <Home/>;
+  const user = true;
+
+  return (
+    <Routes>
+      <Route path='/' element={user ? <Home/> : <Navigate to="/register" replace /> }/>
+      
+      <Route path='/register' element={! user ? <Register/> : <Navigate to="/" replace /> }/>
+      <Route path='/login' element={! user ? <Login/> : <Navigate to="/" replace /> }/>
+      
+      {user && (
+        <>
+          <Route path='/movies' element={ <Home type="movie" /> }/>
+          <Route path='/series' element={ <Home type="series" /> }/>
+
+          <Route path='/watch' element={ <Watch/> }/>
+        </>
+      )}
+
+      <Route path='*' element={<Navigate to="/" replace /> }/>
+    </Routes>
+  );
 };
 
 export default App;
