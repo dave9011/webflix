271834955a8c836b04002a363fe2f51937090045
David Hernandez <hrdavidl@gmail.com>
2022-08-15 02:33:28 -0400

    Create our first chart component, Create user service, Fetch dummy chart data for now

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import { getActiveUserData } from "../../services/api/user";
 import "./home.scss";
 
 const Home = () => {
+    const userData = getActiveUserData();
+
     return (
         <div className="home">
             <FeaturedInfo />
+
+            <Chart data={userData} dataKey="active_users" title="Monthly Active Users" grid />
         </div>
     );
 }
---
+++ admin/src/services/api/user.js
@@ -0,0 +1,32 @@
+export function getActiveUserData() {
+    return [
+        {
+            name: 'Jan',
+            active_users: 4000,
+        },
+        {
+            name: 'Feb',
+            active_users: 3000,
+        },
+        {
+            name: 'Mar',
+            active_users: 2000,
+        },
+        {
+            name: 'Apr',
+            active_users: 2780,
+        },
+        {
+            name: 'May',
+            active_users: 1890,
+        },
+        {
+            name: 'Jun',
+            active_users: 2390,
+        },
+        {
+            name: 'Jul',
+            active_users: 3490,
+        },
+    ];
+}
